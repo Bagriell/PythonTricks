8da78fc9db1c533a1bdf1470e58974d31bb4d0d3
gabriel buetas <gabriel.b@qiiro.eu>
2021-10-31 20:33:26 +0100

    add list comprehension

--- operations.py
+++ operations.py
@@ -13,3 +13,8 @@ result = 1 < 3 < 5                      # chained operators
 result = True if False else True        # Ternary
 if 5 in [1,3,5,7]:                      
     print('True if 5 is in the list')
+
+# Dict Compréhension
+arr = [1, 2, 3, 4, 5]
+a = {arrplusone: arrplusone+1 for arrplusone in arr}
+print(f'a = {a}')  
